acc0e818201f3c0fa69a7059a78ef24e7c17f547
Muhammad Asifur Rahman <asifrahman@arizona.edu>
2025-03-06 20:31:11 -0700

    Initial Commit

---
+++ SobreityTracker/SobreityTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SobreityTrackerApp.swift
+//  SobreityTracker
+//
+//  Created by Muhammad Asifur Rahman on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SobreityTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SobreityTrackerTests/SobreityTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SobreityTrackerTests.swift
+//  SobreityTrackerTests
+//
+//  Created by Muhammad Asifur Rahman on 3/6/25.
+//
+
+import Testing
+@testable import SobreityTracker
+
+struct SobreityTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
